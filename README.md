995aca3ed0e505fabe7d19acee95b232081a64ac
Oscar Granada <oscar.andres.granada@gmail.com>
2019-08-27 18:18:47 -0500

    Updates

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Shelly
+
+A simple command line written in Rust
+
+
+# TO DO:
+
+* Add unit tests.
+* Separate all in data structures like classes.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::io::{stdin, stdout, Write};
 use std::process::{Command};
+use std::process::{exit};
+
+// use std::io;
+use std::path::Path;
+use std::fs;
+
 // use std::result::{Result};
+// use std::mem::{discriminant};
+
+// NON STANDARD
+extern crate prettytable;
+
+use prettytable::{Table, Row, Cell, Attr, color};
+
 
 #[derive(Debug)]
 enum EAction {
@@ -9,15 +22,74 @@ enum EAction {
     NONE
 }
 
-fn execute_command(command: &str) -> EAction {
-    let cmd = String::from(command);
-    if cmd.trim().len() == 0 {
-        return EAction::NONE;
+fn show_directory_info(directory: &Path) -> EAction {
+    let mut table = Table::new();
+    // Add a row per time
+    table.add_row(
+        Row::new(vec![
+            Cell::new("File")
+                .with_style(Attr::Bold)
+                .with_style(Attr::ForegroundColor(color::GREEN)),
+            Cell::new("Type")
+                .with_style(Attr::Bold)
+                .with_style(Attr::ForegroundColor(color::GREEN)),
+            Cell::new("Created")
+                .with_style(Attr::Bold)
+                .with_style(Attr::ForegroundColor(color::GREEN)),
+            Cell::new("Modified")
+                .with_style(Attr::Bold)
+                .with_style(Attr::ForegroundColor(color::GREEN))
+        ])
+    );
+
+    if directory.is_dir() {
+        let elms = fs::read_dir(directory).unwrap();
+        for entry in elms {
+            let file = entry.unwrap();
+            // println!("{:?}", file.path());
+            let pb: std::path::PathBuf = file.path();
+            let meta = pb.metadata().unwrap();
+            let meta_filetype = meta.file_type();
+            let created: String = format!("{:?}", meta.created().unwrap().elapsed().unwrap());
+            let modified: String = format!("{:?}", meta.modified().unwrap().elapsed().unwrap());
+            let filetype = if meta_filetype.is_dir() {
+                "directtory"
+            } else if meta_filetype.is_symlink() {
+                "symlink"
+            } else {
+                "file"
+            };
+            let file_format = if meta_filetype.is_dir() {
+                Attr::ForegroundColor(color::BLUE)
+            } else if meta_filetype.is_symlink() {
+                Attr::ForegroundColor(color::BRIGHT_CYAN)
+            } else {
+                Attr::Dim
+            };
+            // 
+            table.add_row(
+                Row::new(vec![
+                    Cell::new(pb.to_str().unwrap())
+                        .with_style(file_format),
+                    Cell::new(filetype),
+                    Cell::new(created.trim()),
+                    Cell::new(modified.trim())
+                ])
+            );
+        }
     }
-    let mut return_value = match cmd.trim().as_ref() {
-        "exit" => EAction::EXIT,
-        _ => EAction::NONE,
-    };
+    // Print the table to stdout
+    table.printstd();
+
+    EAction::NONE
+}
+
+fn list_directory() -> EAction {
+    show_directory_info(Path::new("."));
+    EAction::NONE
+}
+fn run_subprocess(command: &str) -> EAction {
+    let mut return_value = EAction::NONE;
     let child = Command::new(command).spawn();
     if child.is_ok() {
         let res = child.unwrap().wait();
@@ -32,7 +104,26 @@ fn execute_command(command: &str) -> EAction {
     return_value
 }
 
+fn execute_command(command: &str) -> EAction {
+    let cmd = String::from(command);
+    if cmd.trim().len() == 0 {
+        return EAction::NONE;
+    }
+    let return_value = match cmd.trim().as_ref() {
+        "quit" => EAction::EXIT,
+        "exit" => EAction::EXIT,
+        "ls" => list_directory(),
+        _ => run_subprocess(command),
+    };
+    return_value
+}
+
 fn main() {
+    if cfg!(windows) {
+        println!("this is windows");
+    } else if cfg!(unix) {
+        println!("this is unix alike");
+    }
     let prompt = ">>> ";
     print!("💻  Shelly Terminal\n");
     loop {
@@ -43,9 +134,9 @@ fn main() {
         let command = input.trim();
         let out = execute_command(command);
         match out {
-            EAction::ERROR => {
+            EAction::EXIT => {
                 print!("\nGood Bye!\n");
-                std::process::exit(0);
+                exit(0);
             },
             _ => (/* Nothing to do... */),
         }
